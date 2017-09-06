f0d999ce5389335f0a9fa6f346ff6f8d5d6cd39b
pyronerd62 <rjgoolsby1@gmail.com>
2017-09-06 09:34:23 -0400

    Updated Project to match parent template

--- environments/dev/public_html/admin/index.php
+++ environments/dev/public_html/admin/index.php
@@ -10,16 +10,16 @@ if (isset($_SERVER['HTTP_HOST'])){
 }elseif (isset($_SERVER['SERVER_NAME'])){
     define('Ptech_HOST', str_replace('admin', '', $_SERVER['SERVER_NAME']));
 }
-require(__DIR__ . '/../../vendor/autoload.php');
-require(__DIR__ . '/../../vendor/yiisoft/yii2/Yii.php');
-require(__DIR__ . '/../../common/config/bootstrap.php');
-require(__DIR__ . '/../../backend/config/bootstrap.php');
+require __DIR__ . '/../../vendor/autoload.php';
+require __DIR__ . '/../../vendor/yiisoft/yii2/Yii.php';
+require __DIR__ . '/../../common/config/bootstrap.php';
+require __DIR__ . '/../../backend/config/bootstrap.php';
 
 $config = yii\helpers\ArrayHelper::merge(
-    require(__DIR__ . '/../../common/config/main.php'),
-    require(__DIR__ . '/../../common/config/main-local.php'),
-    require(__DIR__ . '/../../backend/config/main.php'),
-    require(__DIR__ . '/../../backend/config/main-local.php')
+    require __DIR__ . '/../../common/config/main.php',
+    require __DIR__ . '/../../common/config/main-local.php',
+    require __DIR__ . '/../../backend/config/main.php',
+    require __DIR__ . '/../../backend/config/main-local.php'
 );
 
 (new yii\web\Application($config))->run();
